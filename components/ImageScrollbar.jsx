c7a7a7dfcf6c07201a3580e686d498ced6918804
Jayman-Patel2001 <71970156+Jayman-Patel2001@users.noreply.github.com>
2022-05-11 18:23:50 +0530

    Update ImageScrollbar.jsx

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -44,7 +44,7 @@ const ImageScrollbar = ({ data }) => {
       style={{ overflow: "hidden" }}
     >
       {data.map((item) => (
-        <Box width="910px" itemId={item.id} overflow="hidden" p="1">
+        <Box width="910px" itemId={item.id} key={item.id} overflow="hidden" p="1">
           <Image
             placeholder="blur"
             blurDataURL={item.url}
